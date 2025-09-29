7501a6620686546fcf18a174accd5c7854e763df
Filipe Braida <filipebraida@gmail.com>
2025-09-29 11:50:33 -0300

    aula 29/09/2025

--- 2025_2/app/controllers/products_controller.ts
+++ 2025_2/app/controllers/products_controller.ts
@@ -10,14 +10,46 @@ export default class ProductsController {
 
     return view.render('pages/products/index', { products })
   }
+
+  public async show({ params, view }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    return view.render('pages/products/show', { product })
+  }
+
   public async create({ view }: HttpContext) {
     return view.render('pages/products/create')
+  }
+
+  public async edit({ params, view }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    return view.render('pages/products/create', { product })
   }
 
   public async store({ request, response }: HttpContext) {
     const payload = await request.validateUsing(createProductValidator)
 
-    await Product.create(payload)
+    const product = await Product.create(payload)
+
+    return response.redirect().toRoute('products.show', { id: product.id })
+  }
+
+  public async update({ params, request, response }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    const payload = await request.validateUsing(createProductValidator)
+
+    product.merge(payload)
+    await product.save()
+
+    return response.redirect().toRoute('products.show', { id: product.id })
+  }
+
+  public async destroy({ params, response }: HttpContext) {
+    const product = await Product.findOrFail(params.id)
+
+    await product.delete()
 
     return response.redirect().toRoute('products.index')
   }
